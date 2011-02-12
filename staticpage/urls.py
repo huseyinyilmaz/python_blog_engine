c18c50a9ca9f773cefd2151ba59d5ca951584521
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2011-02-13 01:11:27 +0200

    issue-17: started to implement front-end for staticpages  On branch issue_17 	modified:   staticpage/urls.py 	modified:   staticpage/views.py

--- staticpage/urls.py
+++ staticpage/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns,url
 import views
 urlpatterns = patterns('',
-    # url(r'^/(?P<slug>\S+)$',views.index,name='index'),
+    url(r'^/(?P<slug>\S+)$',views.index,name='index'),
     # url(r'^mappingMain/$',views.mappingMain,name='mappingMain'),
     # url(r'^blogs/$',views.blogMain,name='blogMain'),
     # url(r'^flatPages/$',views.flatPagesMain,name='flatPagesMain'),
--- staticpage/views.py
+++ staticpage/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+
+from staticpage.models import StaticPage
+
+def index(request,slug):
+    # TODO We should not show tag list or category list here.
+    # those are in blog level.
+    # but static page is in application level.
+    blog = get_object_or_404(StaticPage,slug=slug)
+    tag_list = blog.tag_set.all()
+    category_list = blog.category_set.all()
+
+    return render_to_response('staticpage/staticpage_index.html',
+                              {'blog':blog,
+                               'date_list':BlogPost.view_objects.date_list(),
+                               'tag_list':tag_list,
+                               'category_list':category_list,
+                               'blogpost_set':BlogPost.view_objects.with_teaser().filter(blog=blog),
+                               },
+                              )
+
