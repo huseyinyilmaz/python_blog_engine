02392cab62978bd6473d4a4c60dfced0e7d7c399
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2011-02-13 23:16:18 +0200

    issue-17: Static page implementation was complated. We can create a static page and view it with /sp/slug/ link.  On branch issue_17 	modified:   staticpage/urls.py 	modified:   staticpage/views.py 	modified:   templates/base.html 	new file:   templates/staticpage/staticpage_index.html

--- staticpage/views.py
+++ staticpage/views.py
@@ -4,19 +4,15 @@ from django.shortcuts import get_object_or_404
 from staticpage.models import StaticPage
 
 def index(request,slug):
-    # TODO We should not show tag list or category list here.
-    # those are in blog level.
-    # but static page is in application level.
-    blog = get_object_or_404(StaticPage,slug=slug)
-    tag_list = blog.tag_set.all()
-    category_list = blog.category_set.all()
+    staticpage = get_object_or_404(StaticPage,slug=slug)
+
+    page = {
+        'title' : staticpage.title,
+        }
+
 
     return render_to_response('staticpage/staticpage_index.html',
-                              {'blog':blog,
-                               'date_list':BlogPost.view_objects.date_list(),
-                               'tag_list':tag_list,
-                               'category_list':category_list,
-                               'blogpost_set':BlogPost.view_objects.with_teaser().filter(blog=blog),
-                               },
+                              {'staticpage':staticpage,
+                               'page':page},
                               )
 
